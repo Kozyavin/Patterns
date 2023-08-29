969e52be8ee99e6c80a5e4632c1291ce3a0058f0
Kozyavin V <kioci@yandex.ru>
2023-08-29 22:20:50 +0300

    ChangeCode2

---
+++ src/main/java/ru/netology/patterntest/DataGenerator.java
@@ -0,0 +1,59 @@
+package ru.netology.patterntest;
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+
+    private DataGenerator() {}
+
+    public static String generateDate(int shift) {
+        // объявление переменной date и задания её значения, для генерации строки с датой
+
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        // объявление переменной city и задания её значения
+        //используем массив валидных городов и класс Random
+
+        var city = new String[]{"Абакан","Анадырь","Архангельск","Барнаул","Владикавказ","Горно-Алтайск","Самара","Хабаровск"};
+        return city[new Random().nextInt(city.length)];
+    }
+
+    public static String generateName(String locale) {
+
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            // создание пользователя user с использованием методов generateCity(locale),generateName(locale),generatePhone(locale)
+
+            return new UserInfo(generateCity(locale),generateName(locale),generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
--- src/test/java/ru/netology/patterntest/MeetingPlanning.java
+++ src/test/java/ru/netology/patterntest/MeetingPlanning.java
@@ -1,101 +1,77 @@
 package ru.netology.patterntest;
 
 import com.codeborne.selenide.Condition;
-
 import com.github.javafaker.Faker;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.Keys;
-
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MeetingPlanning {
-        Faker faker;
+    Faker faker;
 
-        @BeforeEach
-        void setUpAll() {
-            faker = new Faker(new Locale("ru"));
-        }
-
-        @ParameterizedTest
-        @CsvFileSource(files = {"src/test/resources/vacationTownAndName.csv"})
-
-        void positiveTest(String town, String name) {
-
-            SimpleDateFormat reDay = new SimpleDateFormat("dd.MM.yyyy");
-
-            //String address = faker.address().cityName();
-            String data = reDay.format(faker.date().future(1000,3, TimeUnit.DAYS));
-            //String name = faker.name().fullName();
-            String phone = faker.phoneNumber().phoneNumber();
-
-
-
-            open("http://localhost:9999");
-
-            $("[data-test-id='city'] input").setValue(town);
-            $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//очистка поля даты
-            $("[data-test-id='date'] input").setValue(data);
-            $("[data-test-id='name'] input").setValue(name);
-            $("[data-test-id='phone'] input").setValue(phone);
-            $("[data-test-id='agreement'].checkbox").click();
-            $(".button").click();
-
-            $(".notification__content")
-                    .shouldHave(text("Встреча успешно запланирована на " + data), Duration.ofSeconds(10))
-                    .shouldBe(Condition.visible);
-            //Condition - метод , для проверки различных свойств элементов  определённым условиям,в начале вызвать метод should()
-            //Condition.text - проверка, что элемент содержит текст
-            //Condition.visible - проверка, что элемент видимый на странице
-
-        }
-    @Test
-    void positiveTest2() {
-
-        SimpleDateFormat reDay = new SimpleDateFormat("dd.MM.yyyy");
-
-        String address = faker.address().cityName();
-        String data = reDay.format(faker.date().future(10,3, TimeUnit.DAYS));
-        String data2 = reDay.format(faker.date().future(20,11, TimeUnit.DAYS));//2-я дата
-        String name = faker.name().fullName();
-        String phone = faker.phoneNumber().phoneNumber();
-        System.out.println(data);
-
-
+    @BeforeEach
+    void setup() {
         open("http://localhost:9999");
-        //Выбор первой даты встречи
-        $("[data-test-id='city'] input").setValue(address);
+        ;
+    }
+
+    @Test
+    void planingMeetingTest(String town, String name) {
+        DataGenerator.UserInfo User = DataGenerator.Registration.generateUser("ru");//создали объект User с данными
+        int daysAddFirstMeeting = 4;
+        String firstDayMeeting = DataGenerator.generateDate(daysAddFirstMeeting);
+        int daysAddSecondMeeting = 10;
+        String secondDayMeeting = DataGenerator.generateDate(daysAddSecondMeeting);
+
+        $("[data-test-id='city'] input").setValue(DataGenerator.generateCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//очистка поля даты
-        $("[data-test-id='date'] input").setValue(data);//ввод даты(со смещением на +4 дня) с применением метода
-        $("[data-test-id='name'] input").setValue(name);
-        $("[data-test-id='phone'] input").setValue(phone);
+        $("[data-test-id='date'] input").setValue(firstDayMeeting);
+        $("[data-test-id='name'] input").setValue(DataGenerator.generateName("ru"));
+        $("[data-test-id='phone'] input").setValue(DataGenerator.generatePhone("ru"));
         $("[data-test-id='agreement'].checkbox").click();
         $(".button").click();
-        //Ввод другой даты (2-й) при сохранении того же имени, телефона и города
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + firstDayMeeting), Duration.ofSeconds(13))
+                .shouldBe(Condition.visible);
+
+    }
+    @Test
+    void changeMeetingTest(String town, String name) {
+        DataGenerator.UserInfo User = DataGenerator.Registration.generateUser("ru");//создали объект User с данными
+        int daysAddFirstMeeting = 4;
+        String firstDayMeeting = DataGenerator.generateDate(daysAddFirstMeeting);
+        int daysAddSecondMeeting = 10;
+        String secondDayMeeting = DataGenerator.generateDate(daysAddSecondMeeting);
+
+        $("[data-test-id='city'] input").setValue(DataGenerator.generateCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//очистка поля даты
-        $("[data-test-id='date'] input").setValue(data2);//здесь 2-я дата
+        $("[data-test-id='date'] input").setValue(firstDayMeeting);
+        $("[data-test-id='name'] input").setValue(DataGenerator.generateName("ru"));
+        $("[data-test-id='phone'] input").setValue(DataGenerator.generatePhone("ru"));
+        $("[data-test-id='agreement'].checkbox").click();
         $(".button").click();
-        //Появляется предложение переназначить, жмём "Перепланировать". В поле даты, остаётся 2-я выбранныя дата. Жмём
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + firstDayMeeting), Duration.ofSeconds(13))
+                .shouldBe(Condition.visible);
+
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);//очистка поля даты
+        $("[data-test-id='date'] input").setValue(secondDayMeeting);
+        $(".button").click();
+        $(byText("Запланировать")).click();
+
+
+        $("[data-test-id='replan-notification'].notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату.Перепланировать?"), Duration.ofSeconds(13))
+                .shouldBe(Condition.visible);
         $(byText("Перепланировать")).click();
 
         $(".notification__content")
-                .shouldHave(text("Встреча успешно запланирована на " + data2), Duration.ofSeconds(15))
+                .shouldHave(text("Встреча успешно запланирована на " + secondDayMeeting), Duration.ofSeconds(13))
                 .shouldBe(Condition.visible);
-        //Condition - метод , для проверки различных свойств элементов  определённым условиям,в начале вызвать метод should()
-        //Condition.text - проверка, что элемент содержит текст
-        //Condition.visible - проверка, что элемент видимый на странице
 
     }
-    }
-
+}
