9f655879365bf7b209a576f1956d4c2056e1b5c3
Kozyavin V <kioci@yandex.ru>
2023-09-24 23:47:19 +0300

    AddAllureReport

--- src/test/java/ru/netology/patterntest/MeetingPlanning.java
+++ src/test/java/ru/netology/patterntest/MeetingPlanning.java
@@ -1,7 +1,13 @@
 package ru.netology.patterntest;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -12,12 +18,25 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class MeetingPlanning {
 
+
+
+    @BeforeAll
+    //прослушиваем Selenide и вызываем экземпляр класса AllureSelenide()
+    static void setUpAll(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() { //закрытие процесса
+            SelenideLogger.removeListener("allure");
+        }
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
 
     }
     @Test
+    @Owner("Vladimir K. - QA ingener") //указание владельца(кто писал) теста
+    @Description("Выполняется заполнение всех полей формы для проверки функции пеерпланирования встречи")
     void changeMeetingTest() {
         DataGenerator.UserInfo user = DataGenerator.Registration.generateUser("ru");//создали объект User с данными
         int daysAddFirstMeeting = 4;
